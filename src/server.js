d39885c8e8502dd7474aa37c1767e13f9bb14372
DiegoCuba <diego.havanatur@gmail.com>
2020-08-22 16:28:39 -0400

    v 1.0

---
+++ src/server.js
@@ -0,0 +1,57 @@
+require('dotenv').config()
+
+const express = require('express')
+const app = express()
+const nodemailer = require('nodemailer')
+const cors = require('cors')
+const http = require('http')
+const env = process.env
+
+
+app.use(cors())
+
+const Transport = nodemailer.createTransport({
+    service: env.EMAIL_SERVICE,
+    host: env.EMAIL_HOST,
+    secure: env.EMAIL_SECURE,
+    port: env.EMAIL_PORT,
+    auth: {
+        user: env.GMAIL_USER_NAME,
+        pass: env.GMAIL_USER_PASSWORD
+    }
+})
+
+const texto = `Mensaje OK enviado a las ${new Date().toDateString()}`
+const mail = {
+    from: '"Social"<social.bot.secure@gmail.com>',
+    to: 'diego.netsolutions@gmail.com',
+    subject: 'It Works!!',
+    text: texto
+
+}
+
+app.get('/sendMail', (req, res) => {
+    Transport.sendMail(mail, (error, info) => {
+        if (error) {
+            console.log(error)
+            res.json(error)
+
+        }
+
+        else {
+            console.log('Mail Enviado')
+            console.log(info)
+
+
+            res.json(info)
+        }
+    })
+
+})
+
+const server = http.createServer(app)
+
+server.listen('80', () => {
+    console.log('The server is Runing....')
+
+})
